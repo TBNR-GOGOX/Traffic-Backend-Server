e8cdcdb5f24ccd2efdf9bae3ef099360060568c0
TBNR-GOGOX <alexander_j_john@hotmail.com>
2026-02-13 07:11:15 +0000

    Update and commit index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// Increase payload limit
+// Increase Payload Limit
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
